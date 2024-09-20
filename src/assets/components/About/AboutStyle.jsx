e56118e2506bdbd77790c719226ce7dfb6c036ee
Vitoria <vitoriaac19@gmail.com>
2024-09-20 16:08:38 +0100

    about us

--- src/assets/components/About/AboutStyle.jsx
+++ src/assets/components/About/AboutStyle.jsx
@@ -10,15 +10,28 @@ export const AboutStyle = styled(About)`
     align-items: center;
     text-align: center;
     width: 40%;
-    height: 40vh;
-    border: 1px solid hsl(0,0%,70%);
-    border-radius: 20px;
+    height: 45vh;
+    border: 1.5px solid orange;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px orange;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
-        
+}
 
+h3{
+    position: absolute;
+    top: 4%;
+    font-size: 2rem;
+}
 
+.container-p{
+    width: 79%;
+}
+
+.container-p p{
+    font-size: 1rem;
+    line-height: 1.5rem;
 }
 
 `;
---
+++ src/assets/components/Filtering/Filtering.jsx
@@ -0,0 +1,5 @@
+function Filtering(){
+    return ( <></> );
+}
+
+export default Filtering;
